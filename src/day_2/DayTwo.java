7c1e564d5aad7f8c7d4f58f1c34a245e9ce1b026
solomon Abate <solomonabate18@gmail.com>
2024-12-04 19:13:55 +0300

    Day 2 part 2

---
+++ src/day_2/DayTwo.java
@@ -0,0 +1,52 @@
+package day_2;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DayTwo {
+    public static boolean checkValidIncreasing(List<Integer> numbers){
+        int prev = numbers.getFirst() - 1;
+        for(Integer number: numbers){
+            if(number - prev < 1 || number - prev > 3){
+                return false;
+            }
+            prev = number;
+        }
+        return true;
+    }
+    public static boolean checkValidDecreasing(List<Integer> numbers){
+        int prev = numbers.getFirst() + 1;
+        for(Integer number : numbers){
+            if(prev - number < 1 || prev - number > 3){
+                return false;
+            }
+            prev = number;
+        }
+        return true;
+
+    }
+    public static void main(String[] args) {
+        try{
+            File file = new File("/home/tikursew/Documents/Advent of Code 2024/src/day_2/input.txt");
+            Scanner scanner = new Scanner(file);
+            int totalAnswer = 0;
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String[] stringNums  = line.split(" ");
+                List<Integer> nums = new ArrayList<>();
+                for(var stringNum : stringNums){
+                    nums.add(Integer.parseInt(stringNum));
+                }
+                if(checkValidDecreasing(nums) || checkValidIncreasing(nums)){
+                    totalAnswer++;
+                }
+            }
+            System.out.println(totalAnswer);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
